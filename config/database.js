d76dbc8415dcdefd466e505d5968f57a9afc60b9
Sobhit Kumar <sobhit@tadnyasoftech.com>
2019-03-14 23:21:41 +0530

    add backend file

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+database: 'mongodb://localhost:27017/socket_io_db',
+secret: 'this124randomsecretkeyhjhjhjhjhfgfg'
+}
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+const users = require('./routes/users');
+
+const app= express();
+mongoose.connect(config.database);
+
+mongoose.connection.on('connected', () => {
+    console.log('Database connected '+ config.database);
+})
+mongoose.connection.on('error', (err) => {
+    console.log('database connection error'+ err);
+})
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport')(passport);
+app.use('/users', users);
+
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoint');
+});
+app.listen(7001, () => {
+    console.log(`Server started at port 7001`);
+});
